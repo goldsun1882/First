0c4386324a163c07714c2faee4061c8667a11f3e
goldsun1882 <jinhuai123@163.com>
2017-10-23 12:02:29 +0800

    代码提交

--- src/main/java/com/jhr/git/First/App.java
+++ src/main/java/com/jhr/git/First/App.java
@@ -4,4 +4,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("this is first maven git project!!");
     }
+
+    private void test(){
+        System.out.println("add private method!!!");
+    }
 }
+
