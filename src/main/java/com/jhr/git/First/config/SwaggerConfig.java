9ddd1da5ed10d49e7c8fab655fc290f22f7860b9
goldsun1882 <jinhuai123@163.com>
2017-11-28 17:42:35 +0800

    测试yml中使用pom配置

--- src/main/java/com/jhr/git/First/config/SwaggerConfig.java
+++ src/main/java/com/jhr/git/First/config/SwaggerConfig.java
@@ -2,20 +2,36 @@ package com.jhr.git.First.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
-    public Docket swaggerSpringMvcPlugin() {
-        ApiInfo apiInfo = new ApiInfo("sample of springboot", "sample of springboot", null, null, null, null, null);
-        return new Docket(DocumentationType.SWAGGER_2).select().paths(regex("/user/.*")).build()
-                .apiInfo(apiInfo).useDefaultResponseMessages(false);
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jhr"))
+                .paths(PathSelectors.any())
+                .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("服务")
+                .description("库存描述")
+                .termsOfServiceUrl("http://blog.didispace.com/")
+                .contact("jhr")
+                .version(SystemConfig.getServiceVersion())
+                .build();
+    }
+
 }
---
+++ src/main/java/com/jhr/git/First/config/SystemConfig.java
@@ -0,0 +1,30 @@
+package com.jhr.git.First.config;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+/**
+ * author: jinhuari
+ * date: 2017/11/28
+ * description:
+ */
+@Repository
+public class SystemConfig implements EnvironmentAware {
+    private static Environment environment;
+
+    private static String serviceVersion;
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+
+    public static String getServiceVersion() {
+        if (StringUtils.isEmpty(serviceVersion) && environment != null && !(StringUtils.isEmpty(environment.getProperty("service.version")))) {
+            SystemConfig.serviceVersion = environment.getProperty("service.version");
+        }
+        return serviceVersion;
+    }
+}
